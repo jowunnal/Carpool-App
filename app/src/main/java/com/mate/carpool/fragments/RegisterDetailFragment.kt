fcaf2d9a1416ff3b505f466a690c0e02cf2fd5bf
seungyunpark <55783172+sypark95@users.noreply.github.com>
2022-11-12 23:46:03 +0900

    Add : navigation ui / register detail 추가

--- app/src/main/java/com/mate/carpool/fragments/RegisterDetailFragment.kt
+++ app/src/main/java/com/mate/carpool/fragments/RegisterDetailFragment.kt
@@ -1,30 +1,25 @@
-package com.mate.carpool
+package com.mate.carpool.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import com.mate.carpool.R
 
-class LoginFragment : Fragment() {
+class RegisterDetailFragment : Fragment() {
+
+    //todo_sypark 이부분 부터 작업 진행해주시면 됩니다. 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_register_detail, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val btnLogin = view.findViewById<Button>(R.id.btn_login)
-
-        btnLogin.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment())
-        }
     }
-
 }
