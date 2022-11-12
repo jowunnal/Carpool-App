92ec5f6cae40d8cdaa63807987c507d1deaf9da3
g2da <hon9g2@gmail.com>
2022-11-12 23:29:07 +0900

    Add : navigation ui / LoginFragment 수정

--- app/src/main/java/com/mate/carpool/LoginFragment.kt
+++ app/src/main/java/com/mate/carpool/LoginFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class LoginFragment: Fragment() {
-    // todo_sypark 여기서 작업 하시면 됩니다.
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
--- app/src/main/java/com/mate/carpool/SplashActivity.kt
+++ app/src/main/java/com/mate/carpool/SplashActivity.kt
@@ -12,13 +12,12 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        //todo_sypark Handler() deprecated 된 소스 --표시가 보이 실텐데 일단 Loop.getMainLooper()로 해서 사용하시고 나중에 기회되시면 coroutine delay() 함수를 사용하시는게 직관적으로 보기 편할 듯합니다.
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
             finish()
-        }, 3000)    //todo_sypark 의미 없는 변수명 3초 지연시키고 실행시킨다는 것이기때문에 그냥 3000 이렇게 박아도 무방합니다.
+        }, 3000)
 
     }
 
