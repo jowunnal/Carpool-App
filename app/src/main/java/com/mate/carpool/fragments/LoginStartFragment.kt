0d51d61372cb17dba61319d4da2373fd861bdd6e
sypark1995 <115516297+sypark1995@users.noreply.github.com>
2022-11-18 16:50:03 +0900

    Add : login ui 추가 완료

--- app/src/main/java/com/mate/carpool/fragments/LoginStartFragment.kt
+++ app/src/main/java/com/mate/carpool/fragments/LoginStartFragment.kt
@@ -13,13 +13,13 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mate.carpool.R
 
-class LoginFragment : Fragment() {
+class LoginStartFragment  : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_login_start, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,19 +28,7 @@ class LoginFragment : Fragment() {
         val btnLogin = view.findViewById<Button>(R.id.btn_confirm)
 
         btnLogin.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment1())
-        }
 
-        val loginString = "로그인 하기"
-        val content = SpannableString(loginString)
-        content.setSpan(UnderlineSpan(), 0, loginString.length, 0)
-
-        view.findViewById<TextView>(R.id.text_login_start).text = content
-
-        val layoutLoginStart = view.findViewById<LinearLayout>(R.id.layout_login_start)
-
-        layoutLoginStart.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToLoginStartFragment())
         }
     }
 
