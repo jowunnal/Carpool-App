061ed0aa1915df89458936d2a3d53bfa9835f1c9
jino <whangwan@gmail.com>
2022-12-16 21:50:36 +0900

    feat : api 통신위한 module 추가

---
+++ app/src/main/java/com/mate/carpool/data/module/RetrofitModule.kt
@@ -0,0 +1,59 @@
+package org.jinhostudy.swproject.api
+
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+import javax.inject.Singleton
+class HeaderInterceptor constructor(private val token: String) : Interceptor {
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val token = "Bearer $token"
+        val newRequest = chain.request().newBuilder()
+            .addHeader("Authorization", token)
+            .build()
+        return chain.proceed(newRequest)
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient():OkHttpClient{
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }).addInterceptor(HeaderInterceptor(""))
+            .build()
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient):Retrofit{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
+            .client(okHttpClient)
+            .baseUrl("")
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun createRestaurantService(retrofit: Retrofit):APIService{
+        return retrofit.create(APIService::class.java)
+    }
+
+}
