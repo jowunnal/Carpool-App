f6c32b48302b2bc854d272437e33845084405f32
jino <whangwan@gmail.com>
2022-12-28 17:06:40 +0900

    fix : bindingAdapter us2,us5 수정, utils 수정

--- app/src/main/java/com/mate/carpool/data/utils/ButtonCheckUtils.kt
+++ app/src/main/java/com/mate/carpool/data/utils/ButtonCheckUtils.kt
@@ -1,6 +1,10 @@
 package com.mate.carpool.data.utils
 
 object ButtonCheckUtils {
+    /*
+    입력된 문자열(inputString)이 패턴(pattern)과 일치하고, maxLength 보다작고, minLength 보다 큰 기준에 부합하면 false 를 반환
+    그렇지않으면 true 반환
+     */
     @JvmStatic
     fun checkRegisterInfoIsCorrect(inputString: String, pattern:String, textMaxLength:Int, textMinLength:Int):Boolean{
         var isTrue = true
---
+++ app/src/main/java/com/mate/carpool/data/utils/SettingToolbarUtils.kt
@@ -0,0 +1,20 @@
+package com.mate.carpool.data.utils
+
+import android.widget.Toolbar
+import androidx.fragment.app.FragmentActivity
+import com.mate.carpool.R
+
+object SettingToolbarUtils {
+    /*
+    해당 fragmentActivity 내에 있는 toolbar 에 ' < ' 모양의 메뉴가 있는 action bar 생성
+     */
+    @JvmStatic
+    fun setActionBar(fragmentActivity: FragmentActivity, toolbar: Toolbar){
+        fragmentActivity.setActionBar(toolbar)
+        fragmentActivity.actionBar!!.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
+            setDisplayShowTitleEnabled(false)
+        }
+    }
+}
