05aed013aed599a2505913aece075281cd84de1a
jino <whangwan@gmail.com>
2022-12-29 19:11:37 +0900

    feat : 기기 화면 넓이와 높이 구하는 utils

---
+++ app/src/main/java/com/mate/carpool/data/utils/LayoutParamsUtils.kt
@@ -0,0 +1,28 @@
+package com.mate.carpool.data.utils
+
+import android.util.DisplayMetrics
+import androidx.fragment.app.FragmentActivity
+
+object LayoutParamsUtils {
+    @JvmStatic
+    fun getBottomSheetDialogDefaultHeight(rate:Int,activity: FragmentActivity): Int {
+        return getWindowHeight(activity) * rate / 100
+    }
+
+    private fun getWindowHeight(activity:FragmentActivity): Int {
+        val displayMetrics = DisplayMetrics()
+        activity.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.heightPixels
+    }
+
+    @JvmStatic
+    fun getBottomSheetDialogDefaultWidth(rate:Int,activity: FragmentActivity): Int {
+        return getWindowWidth(activity) * rate / 100
+    }
+
+    private fun getWindowWidth(activity:FragmentActivity): Int {
+        val displayMetrics = DisplayMetrics()
+        activity.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.widthPixels
+    }
+}
